c8214a25496878438c65b3f0e6f7397f42379fb2
Amirmohammad19 <CRAZYJOKER1616@GMAIL.COM>
2026-07-01 23:07:09 +0330

    Mission 005 - User Input

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome to Atlas OS")
+print("Founder: Amirmohammad")
+print("Version: 0.0.1")
+print("Mission Started 🚀")
---
+++ user.py
@@ -0,0 +1,13 @@
+name = input("What's your name? : ")
+
+goal = input("What's your biggest goal? : ")
+
+print()
+
+print("Welcam", name)
+
+print("your Goal is :", goal)
+
+print()
+
+print("Atlas OS Activated 🚀")
