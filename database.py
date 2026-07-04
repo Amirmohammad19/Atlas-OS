4361856acb8c21b74c8d6ac7f3fec54455c3344e
Amirmohammad19 <CRAZYJOKER1616@GMAIL.COM>
2026-07-04 23:48:09 +0330

    Refactored Database class

--- database.py
+++ database.py
@@ -3,42 +3,61 @@ class Database:
         self.users = []
 
     def add_user(self, user):
+        if self.user_exists(user.user_id):
+            return False
+
         self.users.append(user)
+        return True
+
+    def _find_user_index(self, user_id):
+        return next(
+            (i for i, u in enumerate(self.users) if u.user_id == user_id),
+            -1
+        )
 
     def get_users(self):
-        return self.users
+        return self.users.copy()
 
     def get_user_by_id(self, user_id):
-        for user in self.users:
-            if user.user_id == user_id:
-                return user
-        return None
-
-    def remove_user(self, user_id):
-        user = self.get_user_by_id(user_id)
-        if user:
-            self.users.remove(user)
-            return True
-        return False
-
-    def update_username(self, user_id, new_username):
-        user = self.get_user_by_id(user_id)
-        if user:
-            user.username = new_username
-            return True
-        return False
+        return next(
+            (u for u in self.users if u.user_id == user_id),
+            None
+        )
 
     def get_user_by_username(self, username):
-        for user in self.users:
-            if user.username == username:
-                return user
-        return None
+        return next(
+            (u for u in self.users if u.username == username),
+            None
+        )
+
+    def remove_user(self, user_id):
+        index = self._find_user_index(user_id)
+
+        if index == -1:
+            return False
+
+        self.users.pop(index)
+        return True
+
+    def update_username(self, user_id, new_username):
+        existing = self.get_user_by_username(new_username)
+
+        if existing and existing.user_id != user_id:
+            return False
+
+        user = self.get_user_by_id(user_id)
+
+        if not user:
+            return False
+
+        user.username = new_username
+        return True
 
     def user_exists(self, user_id):
-        return self.get_user_by_id(user_id) is not None
+        return self._find_user_index(user_id) != -1
 
     def get_user_count(self):
         return len(self.users)
-    
+
     def clear_users(self):
         self.users.clear()
